3938b6c333824b56abcdc2bd79aa7a9e840d2136
amgad-ubuntu <amgad.abdulrazzaq@gmail.com>
2025-04-26 18:36:22 +0700

    setup firebase Auth

--- backend/src/config/firebaseConfig.js
+++ backend/src/config/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { FIREBASE_SERVICE_ACCOUNT_KEY } from "./envConfig.js";
+import { getAuth } from "firebase-admin/auth";
 
 const serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
 
@@ -8,10 +9,5 @@ const firebaseApp = initializeApp({
   credential: cert(serviceAccount),
 });
 export const db = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
 
-// const usersCollection = db.collection("users");
-// await usersCollection.get().then((snapshot) => {
-//   snapshot.forEach((doc) => {
-//     console.log(doc.data());
-//   });
-// });
