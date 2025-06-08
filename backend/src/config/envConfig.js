619df0ec782ee364fb1c2aa6114b4072ae25e26f
amgad-Ubuntu <amgad.abdulrazzaq@gmail.com>
2025-06-08 18:52:25 +0700

    test

--- backend/src/config/envConfig.js
+++ backend/src/config/envConfig.js
@@ -2,15 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 // Load environment variables from .env file
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const FIREBASE_SERVICE_ACCOUNT_KEY = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-const GC_CREDENTIAL = process.env.GC_CREDENTIAL;
+const GC_CREDENTIAL_PATH = process.env.GC_CREDENTIAL_PATH;
 const GC_BUCKET_NAME = process.env.GC_BUCKET_NAME;
+
 export {
   PORT,
   GEMINI_API_KEY,
   FIREBASE_SERVICE_ACCOUNT_KEY,
-  GC_CREDENTIAL,
+  GC_CREDENTIAL_PATH,
   GC_BUCKET_NAME,
 };
--- backend/src/config/gcStorageConfig.js
+++ backend/src/config/gcStorageConfig.js
@@ -1,11 +1,17 @@
-import { Storage } from "@google-cloud/storage";
-import { GC_CREDENTIAL, GC_BUCKET_NAME } from "./envConfig.js";
+import { Storage} from "@google-cloud/storage";
+import { GC_CREDENTIAL_PATH, GC_BUCKET_NAME } from "./envConfig.js";
+import fs from "fs";
 
-const credentials = JSON.parse(GC_CREDENTIAL);
-
+// Load Google Cloud credentials from the specified path
+if (!GC_CREDENTIAL_PATH) {
+  throw new Error("GC_CREDENTIAL_PATH is not defined in the environment variables.");
+}
+// const GC_CREDENTIAL = fs.readFileSync(GC_CREDENTIAL_PATH, "utf8");
+// const serviceAccount = JSON.parse(GC_CREDENTIAL);
+// 
 const gcStorage = new Storage({
-  projectId: credentials.projectId,
-  credentials: credentials,
+  projectId: 112,
+  credentials: GC_CREDENTIAL_PATH 
 });
 
 const plants = gcStorage.bucket(GC_BUCKET_NAME);
